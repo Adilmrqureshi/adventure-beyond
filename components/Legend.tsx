cd507ee16bd5a6450424f50f5c59372b51dbf274
Mamunur Qureshi <adilmrqureshi@gmail.com>
2022-12-25 11:39:16 +0000

    update legend

--- components/Legend.tsx
+++ components/Legend.tsx
@@ -2,17 +2,21 @@ import { alternateMedium } from "../utils/fonts";
 
 export const Legend = () => {
   return (
-    <div className="pt-0 p-3">
+    <div className="pt-0">
       <div className="mb-1">
-        <span
-          className={`py-1 px-2 w-[40px] h-[25px] bg-black text-white font-bold text-sm ${alternateMedium.className}`}
-        >
-          <strong>ROLL</strong>
-        </span>{" "}
-        - Roll the die. Ability will have different outcomes, depending on the
-        result of a dice role
+        <i>Some abilities have special conditions:</i>
+        <br />
+        <div className="my-2">
+          <span
+            className={`my- py-1 px-2 w-[40px] h-[25px] bg-black text-white font-bold text-sm ${alternateMedium.className}`}
+          >
+            <strong>ROLL</strong>
+          </span>{" "}
+          - Roll the die. Ability will have different outcomes, depending on the
+          result of a dice role
+        </div>
       </div>
-      <div className="mb-1">
+      <div className="my-2">
         <span
           className={`py-1 px-2 w-[40px] h-[25px] bg-black text-white font-bold text-sm ${alternateMedium.className}`}
         >
@@ -21,7 +25,7 @@ export const Legend = () => {
         - Variable. Ability will have different options to use and different AP
         costs. You will chose yourself which version you want to use.
       </div>
-      <div>
+      <div className="my-2">
         <span
           className={`py-1 px-2 w-[40px] h-[25px] bg-black text-white font-bold text-sm ${alternateMedium.className}`}
         >
